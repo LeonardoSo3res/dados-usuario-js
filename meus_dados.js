8b4b13ab66655674c2ad9cb0f35f21e5c012d3a7
Leonardo Soares <leonardo.soaressantos@hotmail.com>
2026-02-10 11:16:22 -0300

    Add user data collection and greeting in meus_dados.js

---
+++ meus_dados.js
@@ -0,0 +1,9 @@
+let nome;
+let idade;
+let cidade;
+
+nome = prompt("Digite seu nome");
+idade = prompt("Digite sua idade");
+cidade =  prompt("Qual a sua cidade?");
+
+console.log("Olá! Meu nome é " + nome + ", tenho " + idade + " anos e moro na cidade de " + cidade + ".");
